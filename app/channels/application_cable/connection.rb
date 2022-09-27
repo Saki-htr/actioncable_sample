f9d13a58ba0cd7432c2de9ec828e152fdb5944f6
Saki-htr <saki.hattori0116@gmail.com>
2022-09-27 15:22:47 +0900

    9/26(水) 変更分

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    # とあるユーザーが接続したときに呼ばれる
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      # 別の場所で cookies.signed[:user_id]が入っている前提
+      if verified_user = User.find_by(id: cookies.signed[:user_id])
+        verified_user
+      else
+        # ActionCableの接続を切るメソッド
+        reject_unauthorized_connection
+      end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_024835) do
+ActiveRecord::Schema.define(version: 2022_09_27_061831) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
